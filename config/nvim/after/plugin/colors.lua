465c7daaa5b043b937f7d9f2fc058676180df6e4
Manuel Gil <manugildev@gmail.com>
2023-07-06 11:29:07 +0200

    Update nvim and yabai before trying astrovim

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,8 +1,18 @@
 function SetColorScheme(color)
-	color = color or 'onedark'
+	color = color or 'tokyonight'
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end
 
---SetColorScheme()
+require("tokyonight").setup({
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = false, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+  styles = {
+      floats = "transparent", -- style for floating windows
+      sidebars = "transparent", -- style for floating windows
+  }
+})
+
+SetColorScheme()
--- config/nvim/lua/hypermanu/packer.lua
+++ config/nvim/lua/hypermanu/packer.lua
@@ -1,77 +1,80 @@
+vim.cmd.packadd('packer.nvim')
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use('wbthomason/packer.nvim')
+    use("wbthomason/packer.nvim")
 
     -- Telescope
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use({
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        requires = { { "nvim-lua/plenary.nvim" } },
+        extensions = {
+            fzf = {
+                fuzzy = true,                   -- false will only do exact matching
+                override_generic_sorter = true, -- override the generic sorter
+                override_file_sorter = true,    -- override the file sorter
+                case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            }
+        }
     })
 
     -- ColorScheme
---    use({
---        'rose-pine/neovim',
---        as = 'rose-pine',
---        config = function()
---            require('rose-pine').setup({ disable_italics = true })
---            vim.cmd('colorscheme rose-pine')
---        end
---    })
---
---    use({
---      "loctvl842/monokai-pro.nvim",
---      config = function()
---        require("monokai-pro").setup({
---            transparent_background = true,
---            filter = 'machine',
---        })
---        vim.cmd([[colorscheme monokai-pro]])
---      end
---    })
-    use({
-        'navarasu/onedark.nvim',
-        as = 'onedark',
-        config = function()
-            require('onedark').setup({
-                style = 'darker',
-                transparent = true,
-            })
-            require('onedark').load()
-        end
-    })
+    --    use({
+    --        'rose-pine/neovim',
+    --        as = 'rose-pine',
+    --        config = function()
+    --            require('rose-pine').setup({ disable_italics = true })
+    --            vim.cmd('colorscheme rose-pine')
+    --        end
+    --    })
+    --
+    --    use({
+    --      "loctvl842/monokai-pro.nvim",
+    --      config = function()
+    --        require("monokai-pro").setup({
+    --            transparent_background = true,
+    --            filter = 'machine',
+    --        })
+    --        vim.cmd([[colorscheme monokai-pro]])
+    --      end
+    --    })
+    --
+
+    use({ "folke/tokyonight.nvim" })
 
     --use({ 'manugildev/vim-material-monokai', as = 'material-monokai' })
     --vim.cmd('colorscheme material-monokai')
 
     -- TreeSitter
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('theprimeagen/harpoon')
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("theprimeagen/harpoon")
     -- use({ 'nvim-treesitter/playground' })
     use({
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-        }
+            { "L3MON4D3/LuaSnip" },
+        },
     })
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
